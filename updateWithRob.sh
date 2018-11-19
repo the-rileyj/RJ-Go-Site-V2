7b9b8417996f89b12fcb3549652abc571214a86c
Riley Johnson <rj@therileyjohnson.com>
2018-11-19 16:42:50 +0000

    Removed carriage returns in updating script, added some comments

--- updateWithRob.sh
+++ updateWithRob.sh
@@ -1,7 +1,8 @@
+# Get right directory context
 cd /home/hippea9/RJNewSite
-
+# Pull updates
 git pull
-
+# Kill running server instance
 sudo rob kill
-
+# Build the server and run it if building succeeds
 sudo rob build --root && sudo nohup rob run &
